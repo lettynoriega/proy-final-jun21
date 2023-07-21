b267441ba023912a8302c364ba708f8f3ffff64d
Letty Noriega <leticianoriegarmz@gmmail.com>
2023-07-21 17:25:29 -0600

    mi proyecto JS

---
+++ app.js
@@ -0,0 +1,13 @@
+function cargarCabecero(){
+    let presupuesto=totalIngresos()-totalEgresos();
+    let porcentajeEgreso=totalEgresos()/totalIngresos()
+}
+
+ 
+function totalIngresos(){
+    let totalIngresos=0
+    for(let i = 1; i<=n ; i++){
+    }
+
+}
+
